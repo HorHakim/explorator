7e03682188afc0ea6a69fc686d620023bab974a6
Hakim Horairy <horairyhakim@gmail.com>
2024-03-12 17:08:08 +0100

    getting longest and shortest path

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def prepare_data(edges_df):
 
 	dict_upstream_downstream = {} # key : noeud_amont => value : noeud_aval
 	for _, row in edges_df.iterrows():
-		dict_upstream_downstream[row["noeud_amont"]] = row["noeud_aval"]
+		dict_upstream_downstream[row["noeud_amont"]] = (row["noeud_aval"], row["distance"]) 
 
 	# proposition alternative 1
 	# dict_upstream_downstream = {row["noeud_amont"] : row["noeud_aval"] for _, row in edges_df.iterrows()} 
@@ -21,7 +21,7 @@ def prepare_data(edges_df):
 	return starting_nodes, ending_nodes, dict_upstream_downstream
 
 
-def get_explorators_paths(starting_nodes, ending_nodes, dict_upstream_downstream):
+def get_longest_explorator_path(starting_nodes, ending_nodes, dict_upstream_downstream):
 
 
 	"""
@@ -32,22 +32,41 @@ def get_explorators_paths(starting_nodes, ending_nodes, dict_upstream_downstream
 			je reccupère à l'aide dictionnaire le noeud aval du noeud en cours et je la rajoute à explorator_path
 	"""
 
+	longest_path, max_distance = None, 0
 
+	shortest_path, min_distance = None, float("inf")
+	
 	for starting_node in starting_nodes:
 		# une itération consiste à reconstruire le chemain pris par 1 explorateur	
 		explorator_path = [starting_node]
-		
+		distance = 0
 		while explorator_path[-1] not in ending_nodes:
-			downstream_node = dict_upstream_downstream[explorator_path[-1]]
-			
+			downstream_node = dict_upstream_downstream[explorator_path[-1]][0]
+			distance += dict_upstream_downstream[explorator_path[-1]][1]
+
 			explorator_path.append(downstream_node)
 
-		print(explorator_path)
+		# print(explorator_path, distance)
+		# print("_"*80)
 
+		if distance > max_distance:
+			longest_path = explorator_path
+			max_distance = distance
+
+
+		if distance < min_distance:
+			shortest_path = explorator_path
+			min_distance = distance
+
+	print("le plus grand trajet")
+	print(longest_path, max_distance)
+	print("le trajet le plus court")
+	print(shortest_path, min_distance)
 
 
 if __name__ == "__main__":
 	
 	edges_df = pandas.read_csv("./parcours_explorateurs.csv")
 	starting_nodes, ending_nodes, dict_upstream_downstream = prepare_data(edges_df)
-	get_explorators_paths(starting_nodes, ending_nodes, dict_upstream_downstream)
+	get_longest_explorator_path(starting_nodes, ending_nodes, dict_upstream_downstream)
+	
--- test.py
+++ test.py
@@ -1,34 +1,37 @@
-animals = ["lapin", "chat", "chien", "elephant", "tortue"]
-definitions = ["mignon", "sympa", "sauter", "lourd"]
+# animals = ["lapin", "chat", "chien", "elephant", "tortue"]
+# definitions = ["mignon", "sympa", "sauter", "lourd"]
 
 
-dict_animals_1 = {a : b for a, b in zip(animals, definitions)}
+# dict_animals_1 = {a : b for a, b in zip(animals, definitions)}
 
 
-for element in animals:
-	print(element)
+# for element in animals:
+# 	print(element)
 
-print("_"*60)
+# print("_"*60)
 
-for index in range(len(animals)):
-	print(animals[index])
+# for index in range(len(animals)):
+# 	print(animals[index])
 
-print("_"*60)
+# print("_"*60)
 
-for index, animal in enumerate(animals):
-	print(index)
-	print(animal)
-
-
-
-dict_animals_2 = {"chat": "mignon", "chien": "sympa", "lapin": "sauter", "éléphant":"lourd"}
-
-for animal in dict_animals_2.keys():
-	print(animal)
+# for index, animal in enumerate(animals):
+# 	print(index)
+# 	print(animal)
 
 
-for definition in dict_animals_2.values():
-	print(definition)
 
-for animal, definition in dict_animals_2.items():
-	print(animal, definition)
+# dict_animals_2 = {"chat": "mignon", "chien": "sympa", "lapin": "sauter", "éléphant":"lourd"}
+
+# for animal in dict_animals_2.keys():
+# 	print(animal)
+
+
+# for definition in dict_animals_2.values():
+# 	print(definition)
+
+# for animal, definition in dict_animals_2.items():
+# 	print(animal, definition)
+
+nombres_maisons = 1000
+print(f"il y a {nombres_maisons} maison{"s" if nombres_maisons>1 else ''}")
